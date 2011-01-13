feb63d46b947e44e13c253200f9a159e305ef7d6
dmondark <dmondark@gmail.com>
2011-01-13 17:39:09 +1100

    Set the Default env to "development".

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,7 +9,7 @@ try {
   //Define the db directory (which includes the migrate directory). Both must be created in /path/to/your_li3_app/config/
   define('RUCKUSING_DB_DIR', LITHIUM_APP_PATH . '/config/db');
   //Define the default connection. Point this to a connection (in your app) that you want ruckusing to use by default (ie when not specifying ENV).
-  define('DEFAULT_ENV', 'default');
+  define('DEFAULT_ENV', 'development');
 } catch (ConfigException $e) {
   echo ('Error: '. $e->getMessage());
 }
